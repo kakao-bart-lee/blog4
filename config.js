51f87e4397cfbd9f7c9151dbc29b76c11f379f26
bart <bart.lee@kakaocorp.com>
2020-02-24 03:07:38 +0900

    add about me, resume

--- config.js
+++ config.js
@@ -20,8 +20,8 @@ module.exports = {
       path: '/pages/about'
     },
     {
-      label: 'Contact me',
-      path: '/pages/contacts'
+      label: 'Resume',
+      path: '/pages/resume'
     }
   ],
   author: {
--- content/pages/contacts.md
+++
@@ -1,7 +0,0 @@
----
-title: "Contact me"
-template: "page"
-socialImage: "/media/image-4.jpg"
----
-
-이런 저런 개발자
